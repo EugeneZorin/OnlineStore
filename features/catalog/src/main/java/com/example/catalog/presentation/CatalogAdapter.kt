1def13344e5b9b1d871c2955660770066fec532e
ZubenZitkar <ZubenZitkar@gmail.com>
2024-04-09 19:36:16 +0300

    0.02.8-1
    Signed-off-by: ZubenZitkar <ZubenZitkar@gmail.com>

--- features/catalog/src/main/java/com/example/catalog/presentation/CatalogAdapter.kt
+++ features/catalog/src/main/java/com/example/catalog/presentation/CatalogAdapter.kt
@@ -5,18 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.catalog.R
+import com.example.catalog.databinding.CatalogItemBinding
 import com.example.catalog.databinding.FragmentItemBinding
 
 class CatalogAdapter: RecyclerView.Adapter<CatalogAdapter.ItemHolder>() {
     class ItemHolder(item: View): RecyclerView.ViewHolder(item) {
-        private val binding = FragmentItemBinding.bind(item)
+        private val binding = CatalogItemBinding.bind(item)
         fun bind() = with(binding){
-            fragmentItem
+            testThree
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.catalog_item, parent, false)
         return ItemHolder(view)
     }
 

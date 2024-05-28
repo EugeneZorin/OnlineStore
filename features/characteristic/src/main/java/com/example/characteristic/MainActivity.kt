e1437aaf9c7e70921fd9fb6cc613fbf2c7db6fdf
zuben <Zubenzitkar@gmail.com>
2024-05-28 20:30:38 +0300

    0.05.1

---
+++ features/characteristic/src/main/java/com/example/characteristic/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.characteristic
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.characteristic.databinding.CharacteristicActivityBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: CharacteristicActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = CharacteristicActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+    }
+}

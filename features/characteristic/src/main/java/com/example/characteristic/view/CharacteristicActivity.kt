3257df227b4008404d6f50aa8baa85a44a83848d
zuben <Zubenzitkar@gmail.com>
2024-06-07 16:43:48 +0300

    0.06.7

--- features/characteristic/src/main/java/com/example/characteristic/view/CharacteristicActivity.kt
+++ features/characteristic/src/main/java/com/example/characteristic/view/CharacteristicActivity.kt
@@ -1,9 +1,11 @@
 package com.example.characteristic.view
 
+import android.graphics.Paint
 import androidx.activity.viewModels
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.catalog.entity.CatalogItem
+import com.example.characteristic.R
 import com.example.characteristic.adapter.ImageAdapterCharacteristic
 import com.example.characteristic.viewmodel.CharacteristicViewModel
 import com.example.characteristic.databinding.CharacteristicActivityBinding
@@ -28,26 +30,34 @@ class CharacteristicActivity : AppCompatActivity() {
     private fun info(){
         val catalogItem: CatalogItem? = intent.getParcelableExtra("item")
 
-        with(binding) {
-            title.text = catalogItem?.title
-            subheading.text = catalogItem?.subtitle
-            availableProducts.text = catalogItem?.available.toString()
-            //  BUG
-            rating.rating = catalogItem?.feedback?.rating!!.toFloat()
-            productRating.text = catalogItem.feedback.rating.toString()
-            reviewNumber.text = catalogItem.feedback.count.toString()
+        catalogItem?.let { item ->
+            with(binding) {
+                title.text = item.title
+                subheading.text = item.subtitle
+                availableProducts.text = applicationContext.getString(R.string.available_products, item.available)
 
-            //
+                rating.rating = item.feedback.rating.toFloat()
+                productRating.text = item.feedback.rating.toString()
+                reviewNumber.text = item.feedback.count.toString()
 
-            newPrice.text = catalogItem.price.price
-            oldPrice.text = catalogItem.price.priceWithDiscount
-            discountPercentage.text = catalogItem.price.discount.toString()
+                newPrice.text = item.price.price
+                oldPrice.apply {
+                    text = item.price.priceWithDiscount
+                    paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                }
+                // BUG
+                discountPercentage.text = applicationContext.getString(R.string.discount, item.available)
 
-            buttonText.text = catalogItem.title
-            descriptionTextView.text = catalogItem.description
+                buttonText.text = item.title
+                descriptionTextView.text = item.description
 
-            number.text = catalogItem.info[0].value
+                number.text = item.info.getOrNull(0)?.value ?: ""
+                utilization.text = item.info.getOrNull(1)?.value ?: ""
+                country.text = item.info.getOrNull(2)?.value ?: ""
+                ingredients.text = item.ingredients
+            }
         }
+
     }
 
 

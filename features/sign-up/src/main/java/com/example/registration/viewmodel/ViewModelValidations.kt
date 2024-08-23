03d9d1d191b40decccfce5a0b9ba14d93e29a4e6
zuben <Zubenzitkar@gmail.com>
2024-08-23 16:55:47 +0300

    0.11.4-1

--- features/sign-up/src/main/java/com/example/registration/viewmodel/ViewModelValidations.kt
+++ features/sign-up/src/main/java/com/example/registration/viewmodel/ViewModelValidations.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.registration.contract.ContractFormatPhoneNumber
 import com.example.registration.usecase.validation.DataValidationImpl
+import com.example.registration.usecase.validation.PasswordValidationImpl
 import com.example.registration.validation.DataValidation
+import com.example.registration.validation.PasswordValidationContract
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -13,7 +15,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ViewModelValidations @Inject constructor(
-    private val validationImpl: DataValidation,
+    private val dataValidation: DataValidation,
+    private val passwordValidation: PasswordValidationContract
 ) : ViewModel() {
 
     private val _listenerSymbolsName: MutableLiveData<List<Char>> = MutableLiveData<List<Char>>()
@@ -22,16 +25,33 @@ class ViewModelValidations @Inject constructor(
     private val _listenerSymbolsSurname: MutableLiveData<List<Char>> = MutableLiveData<List<Char>>()
     val listenerSymbolsSurname: MutableLiveData<List<Char>> = _listenerSymbolsSurname
 
+    private val _listenerPasswordCharacter: MutableLiveData<Boolean> = MutableLiveData()
+    val listenerPasswordCharacter: MutableLiveData<Boolean> = _listenerPasswordCharacter
+
+    private val _listenerPasswordSecurity: MutableLiveData<Boolean> = MutableLiveData()
+    val listenerPasswordSecurity: MutableLiveData<Boolean> = _listenerPasswordSecurity
 
     fun validationName(name: String){
         viewModelScope.launch {
-            _listenerSymbolsName.value = validationImpl.validationNameSurname(name)
+            _listenerSymbolsName.value = dataValidation.validationNameSurname(name)
         }
     }
 
     fun validationSurname(surname: String) {
         viewModelScope.launch {
-            _listenerSymbolsSurname.value = validationImpl.validationNameSurname(surname)
+            _listenerSymbolsSurname.value = dataValidation.validationNameSurname(surname)
+        }
+    }
+
+    fun validationPasswordCharacter(password: String) {
+        viewModelScope.launch {
+            _listenerPasswordCharacter.value = passwordValidation.validationPasswordSecurity(password)
+        }
+    }
+
+    fun validationPasswordSecurity(password: String) {
+        viewModelScope.launch {
+            _listenerPasswordSecurity.value = passwordValidation.validationPasswordSecurity(password)
         }
     }
 }
